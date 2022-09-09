907f965a50a7ce3d5748a9cad5a52bf12b5ae250
fluffdroid <benabiofficials@gmail.com>
2022-09-09 11:46:15 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * desription - this is for the main
+ * Return: Always return 0 as success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
