3334331d9e4fab7fb0dc93cb430b910d7c6b8adc
fluffdroid <benabiofficials@gmail.com>
2022-09-09 11:24:01 -0700

    correct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
-/* we are using puts to print result on the screen */
-
 #include <stdio.h>
 
 /**
- * we are using puts to print result on the screen
- *
+ * main - this is the function of the program
+ * description -  we are using puts to print result on the screen
+ * Return: Always 0 (Success)
  */
-
-int main(void) /* this is the main function */
+int main(void)
 {
 	char wo[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(wo);
-	return (0); /* this is the return value */
+	return (0);
 }
