9d98bb88e05fe22bbabad50af5878c7e8c97ef51
fluffdroid <benabiofficials@gmail.com>
2022-09-21 09:41:57 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: this will return 0
+ **/
+int main(void)
+{
+	int sd;
+
+	for (sd = 0; sd < 10; sd++)
+		printf('%i', sd);
+
+	printf('\n');
+	return (0);
+}
