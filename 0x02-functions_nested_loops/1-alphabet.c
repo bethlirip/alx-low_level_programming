d6172f9db0cfc5bde282d3aefa100d378a2dd2c7
Bethlirip <Bj.officials@gmail.com>
2023-03-26 14:07:03 -0700

    alphabet putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,16 +5,8 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+int main(void)
 {
-	char letter = 'a';
-
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
-	_putchar('\n');
 	print_alphabet();
 	return (0);
 }
