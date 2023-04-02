edd6cdd5e4a1b34038e5f8f7b8e286f29f99036d
Bethlirip <Bj.officials@gmail.com>
2023-04-02 12:36:41 -0700

    print most

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,9 +6,9 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for(num = 0; num < 10; num++)
+	for (num = 0; num < 10; num++)
 		if (num != 2 && num != 4)
-		{ 
+		{
 			_putchar(num + '0');
 		}
 	_putchar('\n');
